7ef57fc4c139d28e648732c94adc344f9174533b
komiyamma <komiyama0629@gmail.com>
2021-07-11 23:16:33 +0900

    ファイルの分割をより適切に分割

---
+++ HmNetCOM/HmNetCOMEdit.cs
@@ -0,0 +1,420 @@
+﻿/*
+ * HmNetCOM ver 2.026
+ * Copyright (C) 2021 Akitsugu Komiyama
+ * under the MIT License
+ **/
+
+using System;
+using System.IO;
+using System.Text;
+using System.Reflection;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+
+namespace HmNetCOM
+{
+    internal partial class Hm
+    {
+        public static partial class Edit
+        {
+            /// <summary>
+            /// キー入力があるなどの理由で処理を中断するべきかを返す。
+            /// </summary>
+            /// <returns>中断するべきならtrue、そうでなければfalse</returns>
+            public static bool QueueStatus
+            {
+                get { return pCheckQueueStatus() != 0; }
+            }
+
+            /// <summary>
+            /// 現在アクティブな編集領域のテキスト全体を返す。
+            /// </summary>
+            /// <returns>編集領域のテキスト全体</returns>
+            public static string TotalText
+            {
+                get
+                {
+                    string totalText = "";
+                    try
+                    {
+                        IntPtr hGlobal = pGetTotalTextUnicode();
+                        if (hGlobal == IntPtr.Zero)
+                        {
+                            new InvalidOperationException("Hidemaru_GetTotalTextUnicode_Exception");
+                        }
+
+                        var pwsz = GlobalLock(hGlobal);
+                        if (pwsz != IntPtr.Zero)
+                        {
+                            totalText = Marshal.PtrToStringUni(pwsz);
+                            GlobalUnlock(hGlobal);
+                        }
+                        GlobalFree(hGlobal);
+                    }
+                    catch (Exception)
+                    {
+                        throw;
+                    }
+
+                    return totalText;
+                }
+                set
+                {
+                    SetTotalText(value);
+                }
+            }
+            static partial void SetTotalText(string text);
+
+
+            /// <summary>
+            /// 現在、単純選択している場合、その選択中のテキスト内容を返す。
+            /// </summary>
+            /// <returns>選択中のテキスト内容</returns>
+            public static string SelectedText
+            {
+                get
+                {
+                    string selectedText = "";
+                    try
+                    {
+                        IntPtr hGlobal = pGetSelectedTextUnicode();
+                        if (hGlobal == IntPtr.Zero)
+                        {
+                            new InvalidOperationException("Hidemaru_GetSelectedTextUnicode_Exception");
+                        }
+
+                        var pwsz = GlobalLock(hGlobal);
+                        if (pwsz != IntPtr.Zero)
+                        {
+                            selectedText = Marshal.PtrToStringUni(pwsz);
+                            GlobalUnlock(hGlobal);
+                        }
+                        GlobalFree(hGlobal);
+                    }
+                    catch (Exception)
+                    {
+                        throw;
+                    }
+
+                    return selectedText;
+                }
+                set
+                {
+                    SetSelectedText(value);
+                }
+            }
+            static partial void SetSelectedText(string text);
+
+            /// <summary>
+            /// 現在、カーソルがある行(エディタ的)のテキスト内容を返す。
+            /// </summary>
+            /// <returns>選択中のテキスト内容</returns>
+            public static string LineText
+            {
+                get
+                {
+                    string lineText = "";
+
+                    ICursorPos pos = CursorPos;
+                    if (pos.LineNo < 0 || pos.Column < 0)
+                    {
+                        return lineText;
+                    }
+
+                    try
+                    {
+                        IntPtr hGlobal = pGetLineTextUnicode(pos.LineNo);
+                        if (hGlobal == IntPtr.Zero)
+                        {
+                            new InvalidOperationException("Hidemaru_GetLineTextUnicode_Exception");
+                        }
+
+                        var pwsz = GlobalLock(hGlobal);
+                        if (pwsz != IntPtr.Zero)
+                        {
+                            lineText = Marshal.PtrToStringUni(pwsz);
+                            GlobalUnlock(hGlobal);
+                        }
+                        GlobalFree(hGlobal);
+                    }
+                    catch (Exception)
+                    {
+                        throw;
+                    }
+
+                    return lineText;
+                }
+                set
+                {
+                    SetLineText(value);
+                }
+            }
+            static partial void SetLineText(string text);
+
+            /// <summary>
+            /// CursorPos の返り値のインターフェイス
+            /// </summary>
+            /// <returns>(LineNo, Column)</returns>
+            public interface ICursorPos
+            {
+                int LineNo { get; }
+                int Column { get; }
+            }
+
+            private struct TCursurPos : ICursorPos
+            {
+                public int Column { get; set; }
+                public int LineNo { get; set; }
+            }
+
+            /// <summary>
+            /// MousePos の返り値のインターフェイス
+            /// </summary>
+            /// <returns>(LineNo, Column, X, Y)</returns>
+            public interface IMousePos
+            {
+                int LineNo { get; }
+                int Column { get; }
+                int X { get; }
+                int Y { get; }
+            }
+
+            private struct TMousePos : IMousePos
+            {
+                public int LineNo { get; set; }
+                public int Column { get; set; }
+                public int X { get; set; }
+                public int Y { get; set; }
+            }
+
+            /// <summary>
+            /// ユニコードのエディタ的な換算でのカーソルの位置を返す
+            /// </summary>
+            /// <returns>(LineNo, Column)</returns>
+            public static ICursorPos CursorPos
+            {
+                get
+                {
+                    int lineno = -1;
+                    int column = -1;
+                    int success = pGetCursorPosUnicode(out lineno, out column);
+                    if (success != 0)
+                    {
+                        TCursurPos pos = new TCursurPos();
+                        pos.LineNo = lineno;
+                        pos.Column = column;
+                        return pos;
+                    }
+                    else
+                    {
+                        TCursurPos pos = new TCursurPos();
+                        pos.LineNo = -1;
+                        pos.Column = -1;
+                        return pos;
+                    }
+
+                }
+            }
+
+            /// <summary>
+            /// ユニコードのエディタ的な換算でのマウスの位置に対応するカーソルの位置を返す
+            /// </summary>
+            /// <returns>(LineNo, Column, X, Y)</returns>
+            public static IMousePos MousePos
+            {
+                get
+                {
+                    POINT lpPoint;
+                    bool success_1 = GetCursorPos(out lpPoint);
+
+                    TMousePos pos = new TMousePos
+                    {
+                        LineNo = -1,
+                        Column = -1,
+                        X = -1,
+                        Y = -1,
+                    };
+
+                    if (!success_1)
+                    {
+                        return pos;
+                    }
+
+                    int column = -1;
+                    int lineno = -1;
+                    int success_2 = pGetCursorPosUnicodeFromMousePos(IntPtr.Zero, out lineno, out column);
+                    if (success_2 == 0)
+                    {
+                        return pos;
+                    }
+
+                    pos.LineNo = lineno;
+                    pos.Column = column;
+                    pos.X = lpPoint.X;
+                    pos.Y = lpPoint.Y;
+                    return pos;
+                }
+            }
+
+            /// <summary>
+            /// 現在開いているファイル名のフルパスを返す、無題テキストであれば、nullを返す。
+            /// </summary>
+            /// <returns>ファイル名のフルパス、もしくは null</returns>
+
+            public static string FilePath
+            {
+                get
+                {
+                    IntPtr hWndHidemaru = WindowHandle;
+                    if (hWndHidemaru != IntPtr.Zero)
+                    {
+                        const int WM_USER = 0x400;
+                        const int WM_HIDEMARUINFO = WM_USER + 181;
+                        const int HIDEMARUINFO_GETFILEFULLPATH = 4;
+
+                        StringBuilder sb = new StringBuilder(filePathMaxLength); // まぁこんくらいでさすがに十分なんじゃないの...
+                        IntPtr cwch = SendMessage(hWndHidemaru, WM_HIDEMARUINFO, new IntPtr(HIDEMARUINFO_GETFILEFULLPATH), sb);
+                        String filename = sb.ToString();
+                        if (String.IsNullOrEmpty(filename))
+                        {
+                            return null;
+                        }
+                        else
+                        {
+                            return filename;
+                        }
+                    }
+                    return null;
+                }
+            }
+        }
+    }
+}
+
+
+namespace HmNetCOM
+{
+    internal partial class Hm
+    {
+        public static partial class Edit
+        {
+            static partial void SetTotalText(string text)
+            {
+                string myDllFullPath = System.Reflection.Assembly.GetExecutingAssembly().Location;
+                string myTargetDllFullPath = HmMacroCOMVar.GetMyTargetDllFullPath(myDllFullPath);
+                string myTargetClass = HmMacroCOMVar.GetMyTargetClass(myDllFullPath);
+                HmMacroCOMVar.SetMacroVar(text);
+                string cmd = $@"
+                begingroupundo;
+                selectall;
+                #_COM_NET_PINVOKE_MACRO_VAR = createobject(@""{myTargetDllFullPath}"", @""{myTargetClass}"" );
+                insert member(#_COM_NET_PINVOKE_MACRO_VAR, ""DllToMacro"" );
+                releaseobject(#_COM_NET_PINVOKE_MACRO_VAR);
+                endgroupundo;
+                ";
+                Macro.IResult result = null;
+                if (Macro.IsExecuting)
+                {
+                    result = Hm.Macro.Eval(cmd);
+                } else
+                {
+                    result = Hm.Macro.Exec.Eval(cmd);
+                }
+
+                HmMacroCOMVar.ClearVar();
+                if (result.Error != null)
+                {
+                    throw result.Error;
+                }
+            }
+
+            static partial void SetSelectedText(string text)
+            {
+                string myDllFullPath = System.Reflection.Assembly.GetExecutingAssembly().Location;
+                string myTargetDllFullPath = HmMacroCOMVar.GetMyTargetDllFullPath(myDllFullPath);
+                string myTargetClass = HmMacroCOMVar.GetMyTargetClass(myDllFullPath);
+                HmMacroCOMVar.SetMacroVar(text);
+                string cmd = $@"
+                if (selecting) {{
+                #_COM_NET_PINVOKE_MACRO_VAR = createobject(@""{myTargetDllFullPath}"", @""{myTargetClass}"" );
+                insert member(#_COM_NET_PINVOKE_MACRO_VAR, ""DllToMacro"" );
+                releaseobject(#_COM_NET_PINVOKE_MACRO_VAR);
+                }}
+                ";
+
+                Macro.IResult result = null;
+                if (Macro.IsExecuting)
+                {
+                    result = Hm.Macro.Eval(cmd);
+                }
+                else
+                {
+                    result = Hm.Macro.Exec.Eval(cmd);
+                }
+
+                HmMacroCOMVar.ClearVar();
+                if (result.Error != null)
+                {
+                    throw result.Error;
+                }
+            }
+
+            static partial void SetLineText(string text)
+            {
+                string myDllFullPath = System.Reflection.Assembly.GetExecutingAssembly().Location;
+                string myTargetDllFullPath = HmMacroCOMVar.GetMyTargetDllFullPath(myDllFullPath);
+                string myTargetClass = HmMacroCOMVar.GetMyTargetClass(myDllFullPath);
+                HmMacroCOMVar.SetMacroVar(text);
+                var pos = Edit.CursorPos;
+                string cmd = $@"
+                begingroupundo;
+                selectline;
+                #_COM_NET_PINVOKE_MACRO_VAR = createobject(@""{myTargetDllFullPath}"", @""{myTargetClass}"" );
+                insert member(#_COM_NET_PINVOKE_MACRO_VAR, ""DllToMacro"" );
+                releaseobject(#_COM_NET_PINVOKE_MACRO_VAR);
+                moveto2 {pos.Column}, {pos.LineNo};
+                endgroupundo;
+                ";
+
+                Macro.IResult result = null;
+                if (Macro.IsExecuting)
+                {
+                    result = Hm.Macro.Eval(cmd);
+                }
+                else
+                {
+                    result = Hm.Macro.Exec.Eval(cmd);
+                }
+
+                HmMacroCOMVar.ClearVar();
+                if (result.Error != null)
+                {
+                    throw result.Error;
+                }
+            }
+
+        }
+    }
+}
+
+
+namespace HmNetCOM
+{
+
+    internal static class HmExtentensions
+    {
+        public static void Deconstruct(this Hm.Edit.ICursorPos pos, out int LineNo, out int Column)
+        {
+            LineNo = pos.LineNo;
+            Column = pos.Column;
+        }
+
+        public static void Deconstruct(this Hm.Edit.IMousePos pos, out int LineNo, out int Column, out int X, out int Y)
+        {
+            LineNo = pos.LineNo;
+            Column = pos.Column;
+            X = pos.X;
+            Y = pos.Y;
+        }
+    }
+}
---
+++ HmNetCOM/HmNetCOMMacro.cs
@@ -0,0 +1,186 @@
+﻿/*
+ * HmNetCOM ver 2.026
+ * Copyright (C) 2021 Akitsugu Komiyama
+ * under the MIT License
+ **/
+
+using System;
+using System.IO;
+using System.Text;
+using System.Reflection;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+
+namespace HmNetCOM
+{
+    internal partial class Hm
+    {
+        public static partial class Macro
+        {
+            /// <summary>
+            /// マクロを実行中か否かを判定する
+            /// </summary>
+            /// <returns>実行中ならtrue, そうでなければfalse</returns>
+
+            public static bool IsExecuting
+            {
+                get
+                {
+                    const int WM_USER = 0x400;
+                    const int WM_ISMACROEXECUTING = WM_USER + 167;
+
+                    IntPtr hWndHidemaru = WindowHandle;
+                    if (hWndHidemaru != IntPtr.Zero)
+                    {
+                        IntPtr cwch = SendMessage(hWndHidemaru, WM_ISMACROEXECUTING, IntPtr.Zero, IntPtr.Zero);
+                        return (cwch != IntPtr.Zero);
+                    }
+
+                    return false;
+                }
+            }
+
+            /// <summary>
+            /// マクロをプログラム内から実行した際の返り値のインターフェイス
+            /// </summary>
+            /// <returns>(Result, Message, Error)</returns>
+            public interface IResult
+            {
+                int Result { get; }
+                String Message { get; }
+                Exception Error { get; }
+            }
+
+            private class TResult : IResult
+            {
+                public int Result { get; set; }
+                public string Message { get; set; }
+                public Exception Error { get; set; }
+
+                public TResult(int Result, String Message, Exception Error)
+                {
+                    this.Result = Result;
+                    this.Message = Message;
+                    this.Error = Error;
+                }
+            }
+
+            /// <summary>
+            /// 現在のマクロ実行中に、プログラム中で、マクロを文字列で実行。
+            /// マクロ実行中のみ実行可能なメソッド。
+            /// </summary>
+            /// <returns>(Result, Message, Error)</returns>
+
+            public static IResult Eval(String expression)
+            {
+                TResult result;
+                if (!IsExecuting)
+                {
+                    Exception e = new InvalidOperationException("Hidemaru_Macro_IsNotExecuting_Exception");
+                    result = new TResult(-1, "", e);
+                    return result;
+                }
+                int success = 0;
+                try
+                {
+                    success = pEvalMacro(expression);
+                }
+                catch (Exception)
+                {
+                    throw;
+                }
+                if (success == 0)
+                {
+                    Exception e = new InvalidOperationException("Hidemaru_Macro_Eval_Exception");
+                    result = new TResult(0, "", e);
+                    return result;
+                }
+                else
+                {
+                    result = new TResult(success, "", null);
+                    return result;
+                }
+
+            }
+
+            public static partial class Exec
+            {
+                /// <summary>
+                /// マクロを実行していない時に、プログラム中で、マクロファイルを与えて新たなマクロを実行。
+                /// マクロを実行していない時のみ実行可能なメソッド。
+                /// </summary>
+                /// <returns>(Result, Message, Error)</returns>
+
+                public static IResult File(string filepath)
+                {
+                    TResult result;
+                    if (IsExecuting)
+                    {
+                        Exception e = new InvalidOperationException("Hidemaru_Macro_IsExecuting_Exception");
+                        result = new TResult(-1, "", e);
+                        return result;
+                    }
+                    if (!System.IO.File.Exists(filepath))
+                    {
+                        Exception e = new FileNotFoundException(filepath);
+                        result = new TResult(-1, "", e);
+                        return result;
+                    }
+
+                    const int WM_USER = 0x400;
+                    const int WM_REMOTE_EXECMACRO_FILE = WM_USER + 271;
+                    IntPtr hWndHidemaru = WindowHandle;
+
+                    StringBuilder sbFileName = new StringBuilder(filepath);
+                    StringBuilder sbRet = new StringBuilder("\x0f0f", 0x0f0f + 1); // 最初の値は帰り値のバッファー
+                    IntPtr cwch = SendMessage(hWndHidemaru, WM_REMOTE_EXECMACRO_FILE, sbRet, sbFileName);
+                    if (cwch != IntPtr.Zero)
+                    {
+                        result = new TResult(1, sbRet.ToString(), null);
+                    }
+                    else
+                    {
+                        Exception e = new InvalidOperationException("Hidemaru_Macro_Eval_Exception");
+                        result = new TResult(0, sbRet.ToString(), e);
+                    }
+                    return result;
+                }
+
+                /// <summary>
+                /// マクロを実行していない時に、プログラム中で、文字列で新たなマクロを実行。
+                /// マクロを実行していない時のみ実行可能なメソッド。
+                /// </summary>
+                /// <returns>(Result, Message, Error)</returns>
+                public static IResult Eval(string expression)
+                {
+                    TResult result;
+                    if (IsExecuting)
+                    {
+                        Exception e = new InvalidOperationException("Hidemaru_Macro_IsExecuting_Exception");
+                        result = new TResult(-1, "", e);
+                        return result;
+                    }
+
+                    const int WM_USER = 0x400;
+                    const int WM_REMOTE_EXECMACRO_MEMORY = WM_USER + 272;
+                    IntPtr hWndHidemaru = WindowHandle;
+
+                    StringBuilder sbExpression = new StringBuilder(expression);
+                    StringBuilder sbRet = new StringBuilder("\x0f0f", 0x0f0f + 1); // 最初の値は帰り値のバッファー
+                    IntPtr cwch = SendMessage(hWndHidemaru, WM_REMOTE_EXECMACRO_MEMORY, sbRet, sbExpression);
+                    if (cwch != IntPtr.Zero)
+                    {
+                        result = new TResult(1, sbRet.ToString(), null);
+                    }
+                    else
+                    {
+                        Exception e = new InvalidOperationException("Hidemaru_Macro_Eval_Exception");
+                        result = new TResult(0, sbRet.ToString(), e);
+                    }
+                    return result;
+                }
+            }
+        }
+    }
+}
+
